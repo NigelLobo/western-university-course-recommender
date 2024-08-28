9901a155421dedfdc19ab5d0de3a1374164adf1c
Nigel Lobo <48451374+NigelLobo@users.noreply.github.com>
2024-08-28 00:45:09 +0000

    add more courses

--- syllabi.py
+++ syllabi.py
@@ -18,5 +18,61 @@ Introduction to Computers and Python
 ,Exception Handling
 ,Object-Oriented Programming and Inheritance
     ''',
-    'CS 1027': 'A continuation for both Computer Science 1025A/B and Computer Science 1026A/B. We will study data organization and manipulation, object-oriented principles, abstract data types and their implementations in Java, lists, stacks, queues, trees, recursion, file and memory organization. Course Learning Outcomes,Program in an object-oriented language, namely Java,Create classes and use inheritance for creating sub-classes,Identify and implement arrays and linked data structures,Implement and use abstract data types, i.e. stacks, queues, lists, and trees,Debug code and use exceptions to handle bugs gracefully,Identify the different parts of memory during the execution of a Java program,Program algorithms from pseudo-code Lecture Topics,Object-Oriented Programming Principles,Inheritance and Polymorphism,Exceptions and Debugging,Collections and Generics,Linked Data Structures Stacks, Queues, and Lists Memory Management Recursion Trees,Sorting'
+    'CS 1027': 'A continuation for both Computer Science 1025A/B and Computer Science 1026A/B. We will study data organization and manipulation, object-oriented principles, abstract data types and their implementations in Java, lists, stacks, queues, trees, recursion, file and memory organization. Course Learning Outcomes,Program in an object-oriented language, namely Java,Create classes and use inheritance for creating sub-classes,Identify and implement arrays and linked data structures,Implement and use abstract data types, i.e. stacks, queues, lists, and trees,Debug code and use exceptions to handle bugs gracefully,Identify the different parts of memory during the execution of a Java program,Program algorithms from pseudo-code Lecture Topics,Object-Oriented Programming Principles,Inheritance and Polymorphism,Exceptions and Debugging,Collections and Generics,Linked Data Structures Stacks, Queues, and Lists Memory Management Recursion Trees,Sorting',
+    'CS 1032': '''Techniques used for determining technological needs of businesses; building and managing systems to
+meet those needs; development roles of individuals and organizations; planning and management of
+concepts, personnel and processes; related software tools (spreadsheets, databases).
+This course satisfies the Information Technology requirement for a Certified Accountant, a Certified
+General Accountant or a Certified Management Accountant accreditation. The content of the course
+has been approved by The Institute of Chartered Accountants of Ontario, The Certified General
+Accountants of Ontario and The Society of Management Accountants of Ontario. The materials used in
+this course for the introduction to Microsoft applications software have been approved by Microsoft for
+use in the preparation for the Microsoft Office Specialist Certification exams.''',
+"CS 1033": '''This course is an introduction to multimedia concepts and explores the use of different types of media
+components (text, images, sound, animation and video) to convey ideas and facilitate interaction delivered
+through a web site. Topics include design considerations and media creation/editing tools for creating
+websites, graphics, sound, animation and video files. This course has a theoretical component and a practical
+component. Media components will be studied at a theoretical level, and students will have the opportunity to
+practice their skills by using software to create graphics, animation, movies, and web sites.  There are no
+prerequisites for this course however the student must have their own computer and be familiar with the
+basic skills of computer use and file management. Student will have to purchase some software (under 50
+dollars) in order to complete labs and assignments.
+Learning Outcomes:
+Upon completion of this course, a student will be able to:
+Recognize the characteristics of good design.
+Identify the ways that our world is translated into data that can be stored on a computer.
+List and describe at least 3 compression strategies used by computer scientists.
+Describe how the world wide web and the internet and search engines work.
+List some of the critical people and dates in the history of multimedia.
+Recognize the characteristics that affect the quality and realism of  animation and videos.
+Using software, create and assemble the pieces (images, text, sound, animation and video) to build a
+website.
+LECTURE TOPICS
+1. Intro to Multimedia
+2. Text:
+Text attributes and design considerations
+3. Graphics:
+Types of graphics, attributes, considerations
+Optimization techniques
+File formats
+Graphics software
+4. Web Development:
+Design considerations
+Understanding what's involved in getting your web site up and running
+Web building software 
+5. Animation:
+Types of animation, file formats
+Sources of animation
+Creating animation for the web
+Animation software
+6. Sound:
+Sources of sound, file formats
+Sound-Editing software
+Sound on the web (Audacity)
+7. Video in Multimedia
+Equipment overview
+Digital video considerations
+Video for the web
+''',
+
 }
